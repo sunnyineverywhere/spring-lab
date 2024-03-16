9efcf10427f6007e33597da69a055dfc465035d7
sunnyineverywhere <sunnyinsummer@ewhain.net>
2024-03-16 21:49:36 +0900

    [Toby SpringBoot] 2강까지 코드 정리

--- toby-spring-inflearn/src/main/java/tobyspring/helloboot/HelloController.java
+++ toby-spring-inflearn/src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,2 +1,13 @@
-package tobyspring.helloboot;public class HelloController {
+package tobyspring.helloboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam String name) {
+        return "Hello " + name;
+    }
 }
