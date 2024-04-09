47788c22d8f1033648590f2dd82e17ad7cd9d703
sunnyineverywhere <sunnyinsummer@ewhain.net>
2024-04-09 23:37:12 +0900

    [Toby SpringBoot] 3장 프론트 컨트롤러 추가

--- playground/src/main/java/com/spring/playground/PlaygroundApplication.java
+++ playground/src/main/java/com/spring/playground/PlaygroundApplication.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 import java.io.IOException;
@@ -17,18 +18,25 @@ public class PlaygroundApplication {
     public static void main(String[] args) {
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("hello", new HttpServlet() {
+            servletContext.addServlet("front-controller", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    // super.service(req, resp);
+                    if (req.getRequestURI().equals("/hello")) {
+                        String name = req.getParameter("name");
 
-                    String name = req.getParameter("name");
+                        resp.setStatus(200);
+                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        resp.getWriter().println("Hello " + name);
+                    }
+                    else if (req.getRequestURI().equals("/users")) {
+                        //
+                    }
+                    else {
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
 
-                    resp.setStatus(200);
-                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                    resp.getWriter().println("Hello " + name);
                 }
-            }).addMapping("/hello");
+            }).addMapping("/*");
         });
 
         webServer.start();
