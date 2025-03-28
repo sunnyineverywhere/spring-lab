4632de96388c68ae6665b28a3c4e59af8509e022
sunnyineverywhere <sunnyinsummer@ewhain.net>
2025-03-29 02:12:35 +0900

    [poke-tcg-clone] simulate coin toss

--- poke-tcg-clone/src/main/java/pokemon/clone/Main.java
+++ poke-tcg-clone/src/main/java/pokemon/clone/Main.java
@@ -1,7 +1,17 @@
 package pokemon.clone;
 
+import pokemon.clone.coin.Coin;
+import pokemon.clone.coin.CoinFlipper;
+import pokemon.clone.coin.CoinTossService;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Coin coin = new Coin();
+        CoinFlipper flipper = new CoinFlipper();
+        CoinTossService coinTossService = new CoinTossService(flipper);
+
+        int countUntilTails = coinTossService.flipUntilTails(coin);
+        System.out.println("뒷면이 나오기 전까지 앞면이 나온 횟수: " + countUntilTails);
     }
 }
